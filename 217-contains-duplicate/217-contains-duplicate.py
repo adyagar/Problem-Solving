8c48bbb84fd94d7697b27a5d998ee2e2d01076f2
Aditya Agarwal <ady.agar.aa@gmail.com>
2022-04-01 17:53:35 +0530

    Time: 498 ms (77.17%), Space: 25.9 MB (73.03%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -0,0 +1,12 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        
+        mapp = {}
+        
+        for i in nums:
+            if i in mapp:
+                return True
+            else:
+                mapp[i] = 1
+        return False
